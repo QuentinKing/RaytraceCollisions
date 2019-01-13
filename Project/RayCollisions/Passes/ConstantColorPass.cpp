907a4106b68ac5ba9bd2be7167ef77f609ee1dd9
QuentinKing <quentinsking@gmail.com>
2019-01-13 13:49:41 -0500

    link up pixel shader

--- Project/RayCollisions/Passes/ConstantColorPass.cpp
+++ Project/RayCollisions/Passes/ConstantColorPass.cpp
@@ -25,20 +25,37 @@ bool ConstantColorPass::initialize(RenderContext* pRenderContext, ResourceManage
 	mpResManager = pResManager;
 	mpResManager->requestTextureResource(ResourceManager::kOutputChannel);
 
+	// Create pipeline state and our fullscreen draw call
+	mpGfxState = GraphicsState::create();
+	mpCustomPass = FullscreenLaunch::create("Shaders\\custom.hlsl");
+
     return true;  // Successful initialization.
 }
 
 void ConstantColorPass::renderGui(Gui* pGui)
 {
-	// Add a widget to our GUI to allow us to dynamically change the constant color
-	pGui->addFloat3Var("  Color", mConstColor, 0.0f, 1.0f);
+	// Add widgets in our options panel to fine tune properties during run-time
+	pGui->addFloat3Var("Custom Color", mConstColor, 0.0f, 1.0f);
+	pGui->addFloatVar("Sin Intensity", mScaleValue, 0.0f, 1.0f, 0.000001f);
 }
 
 void ConstantColorPass::execute(RenderContext* pRenderContext)
 {
-	// Get a pointer to a Falcor texture resource of our output channel
-	Texture::SharedPtr outTex = mpResManager->getTexture(ResourceManager::kOutputChannel);
+	// Create a framebuffer object to render to.  Done here once per frame for simplicity, not performance.
+	// This function allows us provide a list of managed texture names, which get combined into an FBO
+	Fbo::SharedPtr outputFbo = mpResManager->createManagedFbo({ ResourceManager::kOutputChannel });
 
-	// Clear the texture to the appropriate color
-	mpResManager->clearTexture(outTex, vec4(mConstColor, 1.0f));
+	// No valid framebuffer?  We're done.
+	if (!outputFbo) return;
+
+	// Set shader parameters.  PerFrameCB is a named constant buffer in our HLSL shader
+	// "PerFrameCB" is the cbuffer object defined somewhere in our shader file
+	auto shaderVars = mpCustomPass->getVars();
+	shaderVars["PerFrameCB"]["gFrameCount"] = mFrameCount++;
+	shaderVars["PerFrameCB"]["gMultValue"] = mScaleValue;
+	shaderVars["PerFrameCB"]["gColor"] = mConstColor;
+
+	// Execute our shader
+	mpGfxState->setFbo(outputFbo);
+	mpCustomPass->execute(pRenderContext, mpGfxState);
 }
